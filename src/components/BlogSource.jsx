18ab301b00522b7d089bc090d454281a17c08943
ericktomaliwan <ericktoma@outlook.com>
2025-06-22 18:20:25 +1000

    components

---
+++ src/components/BlogSource.jsx
@@ -0,0 +1,3 @@
+export function BlogSource (){
+    return 'test';
+}
---
+++ src/components/Card.jsx
@@ -0,0 +1,7 @@
+export function Card(){
+    return <section class="card">
+        <h3>First Name</h3>
+        <p>Description</p>
+        <a href="#">button</a>
+    </section>
+}
