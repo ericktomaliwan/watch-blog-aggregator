9f8cce922c16de0c7a517956e998815dc1f8a0d3
ericktomaliwan <ericktoma@outlook.com>
2025-07-12 21:35:18 +1000

    003 - configure v3 tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import { Card } from "./components/CardProfile";
+//import { Card } from "./components/CardProfile";
 
 export default function App(){
     return (
         <div>
             <h1>Test Root Component</h1>
-            <Card />
         </div>
     );
 }
--- src/components/CardDetails.jsx
+++
@@ -1,9 +0,0 @@
-
-export function CardDetails(props){
-    return (
-        <div>
-            <b>Title</b>
-            <span>{props.title}</span>
-        </div>
-    );
-}
